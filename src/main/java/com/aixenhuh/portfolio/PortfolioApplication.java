da68592c1adc3e0fd64d6c64b5e2084622740c74
aixenhuh@gmail.com <aixenhuh@gmail.com>
2022-03-06 04:15:03 +0900

    오류 수정 1

--- src/main/java/com/aixenhuh/portfolio/PortfolioApplication.java
+++ src/main/java/com/aixenhuh/portfolio/PortfolioApplication.java
@@ -2,12 +2,22 @@ package com.aixenhuh.portfolio;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+import javax.sound.sampled.Port;
 
 @SpringBootApplication
-public class PortfolioApplication {
+public class PortfolioApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
-        SpringApplication.run(PortfolioApplication.class, args);
+        //SpringApplication.run(PortfolioApplication.class, args);
+        SpringApplication app = new SpringApplication(PortfolioApplication.class);
+        app.run(args);
     }
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(PortfolioApplication.class);
+    }
 }
